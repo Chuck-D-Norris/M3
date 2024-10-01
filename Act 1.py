985bb35415ee34a3ff5066e1174a938c3510ea23
Chuck-D-Norris <123090161+Chuck-D-Norris@users.noreply.github.com>
2024-10-01 19:07:33 +0200

    Act 1.py

--- Act 1.py
+++ Act 1.py
@@ -1,27 +1,36 @@
-class Fill():
-    def __init__(self, NomFill):
-        self.NomFill = NomFill
+class Dad:
+    def __init__(self, firstName="Josep", lastName="Fibla Armengol"):
+        self.firstName = firstName
+        self.lastName = lastName
 
-        
+    def getFullName(self):
+        return f"{self.firstName} {self.lastName}"
 
+class Mom:
+    def __init__(self, firstName="Maria", lastName="Agustín Franch"):
+        self.firstName = firstName
+        self.lastName = lastName
 
-class Pare (Fill):
-    def __init__(self,PrimeCognom, segonCognom, Nom,):
-        self.PrimeCognom = PrimeCognom
-        self.segonCognom = segonCognom
-        self.Nom = Nom
+    def getFullName(self):
+        return f"{self.firstName} {self.lastName}"
 
-        
-        
+class Child:
+    def __init__(self, firstName, dad=None, mom=None):
+        self.firstName = firstName
+        self.dad = dad if dad else Dad()
+        self.mom = mom if mom else Mom()
 
+    def getFullName(self):
+        return f"{self.firstName} {self.dad.lastName.split()[0]} {self.mom.lastName.split()[0]}"
 
+    def getDadFullName(self):
+        return self.dad.getFullName()
 
-class Mare(Fill):
-    def __init__(self, PrimeCognom, segonCognom, Nom):
-        self.PrimeCognom = PrimeCognom
-        self.segonCognom = segonCognom
-        self.Nom = Nom
+    def getMomFullName(self):
+        return self.mom.getFullName()
 
-
-Fill = Fill("Xavi")
-print(Fill)
+# Test
+child = Child("Xavi")
+print(child.getFullName())
+print(child.getDadFullName())
+print(child.getMomFullName())
